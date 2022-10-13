9ebb3b40b65d9232afcf3dfe077113a638264a64
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2022-10-13 15:42:17 -0300

    feat: login page

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,14 @@
 import { ButtonHTMLAttributes } from 'react';
 
+import clsx from 'clsx';
+
 export interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> { }
 
-export function Button({ children, type = 'button', ...props }: IButtonProps) {
+export function Button({ children, type = 'button', className, ...props }: IButtonProps) {
   return (
     <button
       type={type}
-      className="py-4 px-3 bg-cyan-500 rounded font-semibold text-black text-sm w-full transition-colors hover:bg-cyan-300 focus:ring-2 ring-gray-100"
+      className={clsx('py-3 px-4 bg-cyan-500 rounded font-semibold text-black text-sm w-full transition-colors hover:bg-cyan-300 focus:ring-2 ring-gray-100', className)}
       {...props}
     >
       {children}
